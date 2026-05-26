906c56deb6f9e194f7ee9e26903fdfdb64abab1c
Defra-Ramkumar <ramkumar.meenakshisundaramdhiruvady@cognizant.com>
2026-05-26 11:35:54 +0100

    document-upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "run_id: $RUN_ID"
-npm run test:browserstack
+npm run test
 
 npm run report:publish
 publish_exit_code=$?
--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -69,7 +69,7 @@ describe('Plain Text Upload Validations', () => {
 })
 
 describe('Clear text button Validations', () => {
-  it('Clear text button for plain text & URL Upload', async () => {
+  it('Clear text button for plain text', async () => {
     await HomePage.navigate()
     await SignInPage.signIn()
     await expect(await HomePage.isLoaded()).toBe(true)
@@ -88,11 +88,11 @@ describe('Clear text button Validations', () => {
     )
 
     await expect(HomePage.getPlainText()).toHaveValue('')
-
+  })
+  it('Clear text button for URL Upload', async () => {
     await HomePage.selectRadioOption('URL upload')
     await HomePage.provideURL('google.co.uk')
     await HomePage.clickClearURLButton()
-
     await expect(HomePage.getUrlTextArea()).toHaveValue('')
   })
 })
@@ -146,6 +146,64 @@ describe('URL Upload Validations', () => {
   })
 })
 
+describe('Document Upload Validations', () => {
+  it('Error message validations-Document upload-No document provided', async () => {
+    await HomePage.navigate()
+    await SignInPage.signIn()
+    await expect(await HomePage.isLoaded()).toBe(true)
+
+    await HomePage.selectRadioOption('File upload')
+
+    await HomePage.clickReviewContent()
+
+    await expect(HomePage.getErrorHeader()).toHaveText('There is a problem')
+
+    await expect(HomePage.getDisplayedDocumentErrorMessage()).toHaveText(
+      'Select a file to upload'
+    )
+
+    await expect(HomePage.getDisplayedErrorSummaryMessage()).toHaveText(
+      'Select a file to upload'
+    )
+  })
+  it('Error message validations-Document upload-Not a valid .docx or .pdf', async () => {
+    CommonUtils.uploadFile('plainTextForValidation.txt')
+    await HomePage.clickReviewContent()
+
+    await expect(HomePage.getDisplayedDocumentErrorMessage()).toHaveText(
+      'The selected file must be a PDF or Word document'
+    )
+
+    await expect(HomePage.getDisplayedErrorSummaryMessage()).toHaveText(
+      'The selected file must be a PDF or Word document'
+    )
+  })
+  it('Error message validations-Document upload-File larger than 10 MB -.docx', async () => {
+    await CommonUtils.uploadFile('largerThan10Mb.docx')
+    await HomePage.clickReviewContent()
+
+    await expect(HomePage.getDisplayedDocumentErrorMessage()).toHaveText(
+      'The selected file must be 10 MB or smaller'
+    )
+
+    await expect(HomePage.getDisplayedErrorSummaryMessage()).toHaveText(
+      'The selected file must be 10 MB or smaller'
+    )
+  })
+  it('Error message validations-Document upload-File larger than 10 MB -.pdf', async () => {
+    await CommonUtils.uploadFile('largerThan10Mb.pdf')
+    await HomePage.clickReviewContent()
+
+    await expect(HomePage.getDisplayedDocumentErrorMessage()).toHaveText(
+      'The selected file must be 10 MB or smaller'
+    )
+
+    await expect(HomePage.getDisplayedErrorSummaryMessage()).toHaveText(
+      'The selected file must be 10 MB or smaller'
+    )
+  })
+})
+
 describe('Text Upload - E2E flow', () => {
   it('Submit & wait for text review to complete', async () => {
     await HomePage.navigate()
@@ -164,7 +222,7 @@ describe('Text Upload - E2E flow', () => {
 
     const row = await HomePage.waitForStatusCompleted(reviewTitle)
 
-    await expect(HomePage.getStatusForRow(row)).toHaveText('Completed')
+    await expect(await HomePage.getStatusForRow(row)).toHaveText('Completed')
 
     const dateTimeText = await HomePage.getDateTimeForRow(row).getText()
 
@@ -207,14 +265,12 @@ describe('Text Upload - E2E flow', () => {
     expect(confirmRowDeleted).toBeNull()
   })
 
-  // WIP
-  it.skip('Submit text for review & verify review page', async () => {
+  it('Submit text for review & verify review page', async () => {
     const reviewContent = CommonUtils.generateAutomationText(
       await CommonUtils.getTextFromFile('plainTextForValidation')
     )
 
     const reviewTitle = reviewContent.split('\n')[0]
-    // const reviewTitle = 'Automation 15/04/2026 11:44:11AM';
     await HomePage.navigate()
     await SignInPage.signIn()
     await expect(await HomePage.isLoaded()).toBe(true)
@@ -225,7 +281,7 @@ describe('Text Upload - E2E flow', () => {
 
     const row = await HomePage.waitForStatusCompleted(reviewTitle)
 
-    await expect(HomePage.getStatusForRow(row)).toHaveText('Completed')
+    await expect(await HomePage.getStatusForRow(row)).toHaveText('Completed')
 
     const dateTimeText = await HomePage.getDateTimeForRow(row).getText()
 
@@ -240,11 +296,10 @@ describe('Text Upload - E2E flow', () => {
 
     await expect(status).toBe('Completed')
 
-    await ReviewResults.verifyCategoryScore('Plain English', '4/5')
-    await ReviewResults.verifyCategoryScore('Clarity & Structure', '4/5')
-    await ReviewResults.verifyCategoryScore('Accessibility', '4/5')
-    await ReviewResults.verifyCategoryScore('GOV.UK Style Compliance', '3/5')
-    await ReviewResults.verifyCategoryScore('Content Completeness', '4/5')
+    await ReviewResults.clickToggle()
+    await ReviewResults.validateScorecard()
+    // await ReviewResults.verifyCategoryScore('Plain English', '4/5')
+    // await ReviewResults.verifyCategoryScore('GOV.UK Style Compliance', '3/5')
 
     await browser.back()
 
@@ -288,7 +343,7 @@ describe('URL Upload - E2E flow', () => {
     const reviewTitle = 'Preparing a flood risk assessment: standing advice'
     const row = await HomePage.waitForStatusCompleted(reviewTitle)
 
-    await expect(HomePage.getStatusForRow(row)).toHaveText('Completed')
+    await expect(await HomePage.getStatusForRow(row)).toHaveText('Completed')
 
     const dateTimeText = await HomePage.getDateTimeForRow(row).getText()
 
@@ -315,6 +370,83 @@ describe('URL Upload - E2E flow', () => {
 
     await HomePage.clickHome()
 
+    let confirmRowDeleted
+
+    await browser.waitUntil(
+      async () => {
+        confirmRowDeleted = await HomePage.getRowByReviewName(reviewTitle)
+        return confirmRowDeleted === null
+      },
+      {
+        timeout: 10000,
+        timeoutMsg: `Row "${reviewTitle}" was still present after delete`
+      }
+    )
+
+    expect(confirmRowDeleted).toBeNull()
+  })
+})
+
+describe('Document Upload - E2E flow', () => {
+  it('Submit & wait for Document review to complete-.docx', async () => {
+    const reviewTitle = 'ValidDoc.docx'
+
+    await HomePage.navigate()
+    await SignInPage.signIn()
+    await expect(await HomePage.isLoaded()).toBe(true)
+
+    await HomePage.selectRadioOption('File upload')
+    await CommonUtils.uploadFile(reviewTitle)
+    await HomePage.clickReviewContent()
+
+    const row = await HomePage.waitForStatusCompleted(reviewTitle)
+    await expect(await HomePage.getStatusForRow(row)).toHaveText('Completed')
+    const dateTimeText = await HomePage.getDateTimeForRow(row).getText()
+    expect(dateTimeText).toMatch(/^\d{2}\/\d{2}\/\d{4},\s\d{2}:\d{2}$/)
+    await HomePage.getViewResultsLink(row).click()
+
+    await expect(browser).toHaveUrl(expect.stringContaining('/review/results/'))
+    await ReviewResults.clickToggle()
+    await ReviewResults.validateScorecard()
+    await browser.back()
+
+    await HomePage.getDeleteLink(row).click()
+    await DeletePage.expectConfirmationHeadingVisible()
+    await DeletePage.cancelAndGoBack()
+    await expect(row).toBeExisting()
+    await HomePage.getDeleteLink(row).click()
+    await DeletePage.expectConfirmationHeadingVisible()
+    await DeletePage.clickConfirmDelete()
+    await HomePage.clickHome()
+  })
+  it('Submit & wait for Document review to complete-.pdf', async () => {
+    const reviewTitle = 'ValidDoc.pdf'
+
+    await HomePage.navigate()
+    await SignInPage.signIn()
+    await expect(await HomePage.isLoaded()).toBe(true)
+
+    await HomePage.selectRadioOption('File upload')
+    await CommonUtils.uploadFile(reviewTitle)
+    await HomePage.clickReviewContent()
+
+    const row = await HomePage.waitForStatusCompleted(reviewTitle)
+    await expect(await HomePage.getStatusForRow(row)).toHaveText('Completed')
+    const dateTimeText = await HomePage.getDateTimeForRow(row).getText()
+    expect(dateTimeText).toMatch(/^\d{2}\/\d{2}\/\d{4},\s\d{2}:\d{2}$/)
+    await HomePage.getViewResultsLink(row).click()
+
+    await expect(browser).toHaveUrl(expect.stringContaining('/review/results/'))
+    await browser.back()
+
+    await HomePage.getDeleteLink(row).click()
+    await DeletePage.expectConfirmationHeadingVisible()
+    await DeletePage.cancelAndGoBack()
+    await expect(row).toBeExisting()
+    await HomePage.getDeleteLink(row).click()
+    await DeletePage.expectConfirmationHeadingVisible()
+    await DeletePage.clickConfirmDelete()
+    await HomePage.clickHome()
     let confirmRowDeleted
 
     await browser.waitUntil(
